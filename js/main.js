adc4aa2102e82bb79a2ff744c2b7875b0bbb72c3
douffy <douffetphilippe@gmail.com>
2013-02-01 22:26:07 +0100

    nettoyage + jquery

--- js/main.js
+++ js/main.js
@@ -3,7 +3,19 @@
 		$("nav select").change(function() {
 			  window.location = $(this).find("option:selected").val();
 		});
-		
+		 
+		 $(".addbasket").click(function (e) {
+	         	e.preventDefault();
+		         $(this).text('Ajouté !');
+		         $('#panier img').attr("src", 'img/panierfull.png');
+		         
+		 });
+		 
+		 $("table td img").click(function (e) {
+	         	e.preventDefault();
+		        $(this).closest('tr').fadeOut();
+		         
+		 });
 		
 	
 }(jQuery));
